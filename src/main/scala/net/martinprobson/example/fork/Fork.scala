4417da80d4aa265275cc651eb520cd22288bcff7
Martin Robson <martin.robson@rakuten.com>
2023-08-29 09:03:58 +0100

    Update

--- src/main/scala/net/martinprobson/example/fork/Fork.scala
+++ src/main/scala/net/martinprobson/example/fork/Fork.scala
@@ -3,12 +3,17 @@ package net.martinprobson.example.async
 import net.martinprobson.example.ZIOApplication
 import zio.*
 
-object Fork extends ZIOApplication:
+object Fork extends ZIOApplication {
 
-  def doWork(n: Int): ZIO[Any, Nothing, Int] =
-    ZIO.logInfo(s"doWork $n") *> ZIO.succeed(n)
+  def doWork(n: Int): UIO[Int] =
+    ZIO.logInfo(s"doWork $n") *> ZIO.succeed(1)
 
-  override def run =
-    ZIO.foreach(1 to 100000)(n => doWork(n).fork).flatMap(f => Fiber.collectAll(f).join)
+  override def run = for {
+    res <- ZIO
+      .foreach(1 to 10000)(n => doWork(n).fork)
+      .flatMap(f => Fiber.collectAll(f).join)
+    s <- ZIO.succeed(res.fold(0)(_ + _))
+    _ <- ZIO.logInfo(s"s = $s")
+  } yield ()
 
-end Fork
+}
